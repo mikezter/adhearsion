5a26b47863f249eb2c58f5ac404110e15f5f7b2c
Ben Klang <ben@alkaloid.net>
2010-10-18 21:26:34 -0400

    Bump AS requirement to 2.3.0; compatible with AS 3.0.0

--- lib/adhearsion/foundation/all.rb
+++ lib/adhearsion/foundation/all.rb
@@ -1,7 +1,7 @@
 require 'English'
 require 'tmpdir'
 require 'tempfile'
-require 'active_support'
+require 'active_support/core_ext'
 
 # Require all other files here.
 Dir.glob File.join(File.dirname(__FILE__), "*rb") do |file|
